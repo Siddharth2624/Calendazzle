c7cce84f1e9d30d777a578a2ddae659593718b88
Siddharth Varshney <siddharthvarshney45@gmail.com>
2025-07-10 18:20:49 +0530

    fix

--- src/app/components/RightNav.tsx
+++ src/app/components/RightNav.tsx
@@ -2,25 +2,23 @@
 
 import Link from "next/link"
 
-export default function RightNav({email}:{email:string}){ {
+export default function RightNav({ email }: { email: string }) {
     const hasLoggedOut = typeof window !== "undefined" && window.location.href.includes('logged-out=1');
-    console.log({hasLoggedOut});
-    if(email && !hasLoggedOut){
-        return(
-            <nav className="flex items-center gap-4">
-                <Link href={"/dashboard"} className="bg-blue-600 text-white py-2 px-2 rounded-full">Dashboard</Link>
-                <a href={'/api/logout'}>Logout</a>
-            </nav>
-        )
-    }
+    console.log({ hasLoggedOut });
 
-    else{
-        return(
+    if (email && !hasLoggedOut) {
+        return (
             <nav className="flex items-center gap-4">
-                <Link href={"/api/auth"}>Sign in</Link>
-                <Link href={"/about"} className="bg-blue-600 text-white py-2 px-2 rounded-full">Get Started</Link>
-            </nav> 
-        )
+                <Link href="/dashboard" className="bg-blue-600 text-white py-2 px-2 rounded-full">Dashboard</Link>
+                <Link href="/api/logout">Logout</Link>
+            </nav>
+        );
+    } else {
+        return (
+            <nav className="flex items-center gap-4">
+                <Link href="/api/auth">Sign in</Link>
+                <Link href="/about" className="bg-blue-600 text-white py-2 px-2 rounded-full">Get Started</Link>
+            </nav>
+        );
     }
-  }
-}   
+}
